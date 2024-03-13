8d0782a9bdae9c6307d6f9506f40eeb424b83987
stork11 <126308978+stork11@users.noreply.github.com>
2024-03-13 19:22:15 +0800

    aaa

--- run.sh
+++ run.sh
@@ -2,23 +2,6 @@
 
 country = $0
 
-# 打开bbr
-bash <(curl -Ls neko.nnr.moe/iiii.sh)
-# 安装Docker
-curl -fsSL https://get.docker.com/ | sh
-# 安装探针
-wget -N --no-check-certificate https://gitlab.com/xx.stork/script/-/raw/master/ws2.sh
-# 安装git
-apt install git -y
-# 下载对应脚本
-mkdir /root/stork
-git clone -b $country https://github.com/stork11/Script.Node.git /root/stork
-
-runXrayrs
-runHysterias
-
-bash /root/ws2.sh
-
 runXrayrs(){
 	xrayrs=$(ls "/root/stork/xrayr")
 	for filename in $xrayrs
@@ -39,3 +22,21 @@ runHysterias(){
 		docker compose up -d
 	done
 }
+
+# 打开bbr
+bash <(curl -Ls neko.nnr.moe/iiii.sh)
+# 安装Docker
+# curl -fsSL https://get.docker.com/ | sh
+# 安装探针
+wget -N --no-check-certificate https://gitlab.com/xx.stork/script/-/raw/master/ws2.sh
+# 安装git
+apt install git -y
+# 下载对应脚本
+mkdir /root/stork
+git clone -b $country https://github.com/stork11/Script.Node.git /root/stork
+
+runXrayrs
+runHysterias
+
+bash /root/ws2.sh
+
