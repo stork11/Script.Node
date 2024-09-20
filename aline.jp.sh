043517c56d83ddb174afff65d7a1965dbbb22326
stork11 <126308978+stork11@users.noreply.github.com>
2024-09-20 13:52:15 +0800

    aaa

--- aline.jp.sh
+++ aline.jp.sh
@@ -2,6 +2,6 @@
 
 wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.Node/master/run.sh" && bash run.sh jp
 wget -O install.sh --no-check-certificate https://pass.wocao.one/client/9CaCNonogdEEIqrf/install.sh && bash install.sh && rm install.sh -f
-docker run -d --name ddns -e ZONE=lovehonor.top -e SUBDOMAIN=sg1 xwhales/ddns:go
+docker run -d --name ddns -e ZONE=lovehonor.top -e SUBDOMAIN=jp1 xwhales/ddns:go
 
 
