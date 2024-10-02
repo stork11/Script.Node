55d90fdabaaa3ce64a2226293fd54ed799e9e9c6
stork11 <126308978+stork11@users.noreply.github.com>
2024-10-03 05:10:39 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 # wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.Node/master/run.sh" && bash run.sh jp
 wget -O install.sh --no-check-certificate https://pass.wocao.one/client/dNhuQSLf2dzQSOrR/install.sh && bash install.sh && rm install.sh -f
-docker run -d --name ddns -e ZONE=lovehonor.top -e SUBDOMAIN=test1 xwhales/ddns:go
+# docker run -d --name ddns -e ZONE=lovehonor.top -e SUBDOMAIN=test1 xwhales/ddns:go
 
 
