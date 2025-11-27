8c02fa8e82412ec31c2e791597f24a259979dfbe
stork11 <126308978+stork11@users.noreply.github.com>
2025-11-27 21:50:58 +0800

    Update aline.nc.sg.sh

--- aline.nc.sg.sh
+++ aline.nc.sg.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.Node/master/run.sh" && bash run.sh nc.sg
-wget -O install.sh --no-check-certificate https://pass.wocao.one/client/7ASrUJOnvQI7jKjh/install.sh && bash install.sh && rm install.sh -f
+wget -O install.sh --no-check-certificate https://pass.wocao.one/client/OEvndFZgXZTg2cxP/install.sh && bash install.sh && rm install.sh -f
 docker run -d --name ddns -e ZONE=lovehonor.top -e SUBDOMAIN=nc-sg1 xwhales/ddns:go
-
-
