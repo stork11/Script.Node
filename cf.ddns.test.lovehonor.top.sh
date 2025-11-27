dd515639afc564e317b5c23d4000100325cf5787
stork11 <126308978+stork11@users.noreply.github.com>
2025-11-28 02:08:17 +0800

    test

---
+++ cf.ddns.test.lovehonor.top.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.Node/master/cf.ddns.sh" && bash cf.ddns.sh 1da7d43cfc0596c1dcc3dfa1848cbe6f test.lovehonor.top 4ee25d5b1daf46d6bdd3d33b0cc90ddb
