ee580e41229e5886a9554bdc8263929632e3e590
stork11 <126308978+stork11@users.noreply.github.com>
2025-11-28 00:45:24 +0800

    aaa

--- aline.nc.sg.sh
+++ aline.nc.sg.sh
@@ -3,3 +3,4 @@
 wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.Node/master/run.sh" && bash run.sh nc.sg
 wget -O install.sh --no-check-certificate https://pass.wocao.one/client/OEvndFZgXZTg2cxP/install.sh && bash install.sh && rm install.sh -f
 docker run -d --name ddns -e ZONE=lovehonor.top -e SUBDOMAIN=nc-sg1 xwhales/ddns:go
+
--- aline.sg.sh
+++ aline.sg.sh
@@ -4,3 +4,4 @@ wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script
 wget -O install.sh --no-check-certificate https://pass.wocao.one/client/7ASrUJOnvQI7jKjh/install.sh && bash install.sh && rm install.sh -f
 docker run -d --name ddns -e ZONE=lovehonor.top -e SUBDOMAIN=sg1 xwhales/ddns:go
 
+
