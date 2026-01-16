fa827ba48cdbb6cf9216b836707d1f7143f656c2
stork11 <126308978+stork11@users.noreply.github.com>
2026-01-16 17:43:15 +0800

    Create cf.ddns.nn.interesting2026.blog.sh

---
+++ cf.ddns.nn.interesting2026.blog.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+
+wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.Node/master/cf.ddns.sh" && bash cf.ddns.sh 34c17951ca9f671682416fa6773d8a50 nn.interesting2026.blog 0a966a5ed956ba93706945cb5a5d57fe 4041c29b4f971d7a0260d67d199ec73f
