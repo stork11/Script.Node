b6757795c6f4b9fe536d3b89219bd8fe230c448b
stork11 <126308978+stork11@users.noreply.github.com>
2026-01-16 17:22:09 +0800

    Create cf.ddns.hk1.lovehonor.top.sh

---
+++ cf.ddns.hk1.lovehonor.top.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.Node/master/cf.ddns.sh" && bash cf.ddns.sh 1da7d43cfc0596c1dcc3dfa1848cbe6f hk1.lovehonor.top 0cea51879e0a675b1b409b8b52f31992 21dec9bb66894580cb31117874bbfbde
