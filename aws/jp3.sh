03dbc6bf7c0b115e0aae7f43b6160ce86bebe25b
stork11 <x.stork@pm.me>
2026-04-03 16:40:36 +0800

    nn2

---
+++ aws/jp3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 加载 BBR 模块
+modprobe tcp_bbr
+
+# 修改内核参数
+sysctl -w net.core.default_qdisc=fq
+sysctl -w net.ipv4.tcp_congestion_control=bbr
+
+# wget -O install.sh --no-check-certificate https://pass.wocao.one/client/9CaCNonogdEEIqrf/install.sh && bash install.sh && rm install.sh -f
+apt update -y
+apt install cron -y
+
+curl -L -o /usr/local/bin/cf.ddns.2.sh https://raw.githubusercontent.com/stork11/Script.Node/master/cf.ddns.nn2.interesting2026.blog.sh && \
+chmod +x /usr/local/bin/cf.ddns.2.sh && /usr/local/bin/cf.ddns.2.sh && \
+(crontab -l 2>/dev/null; echo "* * * * * /usr/local/bin/cf.ddns.2.sh >/dev/null 2>&1") | crontab -
+
+S=nyanpass bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 400ac14a-c6f7-4f0b-9761-7ce8c76235bc -u https://ny.wocao.one"
+S=nyanpass2 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ec2adbf9-b2db-41c5-a9fc-154ee9372fa8 -u https://ny.wocao.one"
---
+++ cf.ddns.nn2.interesting2026.blog.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+
+wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.Node/master/cf.ddns.sh" && bash cf.ddns.sh 34c17951ca9f671682416fa6773d8a50 nn2.interesting2026.blog 6ce51e61388f380d5cd9655bb880ea5b 9646665cad9aab558ec4c513512c649b
